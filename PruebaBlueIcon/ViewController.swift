d8f5506e09549163bf4b15c4212d1e744854b819
Miguel Mexicano Herrera <miguel.mexicano.contractor@bbva.com>
2018-04-04 19:41:17 -0500

    Initial Commit

---
+++ PruebaBlueIcon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PruebaBlueIcon
+//
+//  Created by miguel mexicano on 04/04/18.
+//  Copyright © 2018 miguel mexicano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
